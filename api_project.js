f3dd442a6bc140f822dc4e37dec1d21c64a0657a
Huy Quyen Tran <huyquyent@gmail.com>
2019-10-30 00:40:59 +0700

    First commit

---
+++ api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "Air ticket management",
+  "version": "1.0.0",
+  "description": "Air ticket management project",
+  "title": "Air ticket management",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-10-29T17:34:42.727Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
